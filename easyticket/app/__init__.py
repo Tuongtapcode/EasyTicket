3ef08890dee68c06197c7cb7f198b860c74f16b0
Ngọc Tưởng <cocghe2468@gmail.com>
2025-09-01 12:06:52 +0700

    Revert "Merge pull request #4 from Tuongtapcode/feature/organizer-ui"
    This reverts commit 216716616dafb7dfc0fbba267752e51bec441d12, reversing
    changes made to 8a3abf5b5bc4702b85e7f9933de876d80fdca2d4.

--- easyticket/app/__init__.py
+++ easyticket/app/__init__.py
@@ -1,43 +1,15 @@
 from flask import Flask
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import quote
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-import os
+# from flask_login import LoginManager
+# from datetime import datetime
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", "dev-secret-change-me")
-
-RAILWAY_USER = os.getenv("DB_USER", "root")
-RAILWAY_PASSWORD = os.getenv("DB_PASSWORD", "KYrheRlKRriAUqwhMBcJFlxlItWEMPMB")
-RAILWAY_HOST = os.getenv("DB_HOST", "shinkansen.proxy.rlwy.net")
-RAILWAY_PORT = int(os.getenv("DB_PORT", 27884))
-RAILWAY_DB   = os.getenv("DB_NAME", "railway")
-
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    f"mysql+pymysql://{RAILWAY_USER}:{quote(RAILWAY_PASSWORD)}@{RAILWAY_HOST}:{RAILWAY_PORT}/{RAILWAY_DB}?charset=utf8mb4"
-)
-
-
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'HJGFGHF^&%^&&*^&*YUGHJGHJF^%&YYHB'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/ticketdb?charset=utf8mb4" % quote(
+    'Admin@123')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = "auth.login"
-migrate = Migrate(app, db)
+# login = LoginManager(app)
 
-#import tất cả models để Alembic detect
-from app.models import (
-    User, Category, EventType, Event,
-    TicketType, Ticket, Order, OrderDetail,
-    Payment
-)
-
-from app.templates.blueprints.auth import auth
-app.register_blueprint(auth)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
--- easyticket/app/index.py
+++ easyticket/app/index.py
@@ -1,24 +1,11 @@
-from flask_login import current_user, login_required
-
 from app import dao
 from app import app
-from flask import render_template, abort
-
-from app.models import UserRole
-
-
+from flask import render_template
 @app.route('/')
 def index():
-    return render_template("user/index.html")
+    events = dao.load_event()
+    return render_template("index.html", events=events)
 
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    print("current_user:", current_user)
-    print("role:", current_user.user_role, type(current_user.user_role))
-    if current_user.user_role != UserRole.ORGANIZER:
-        abort(403)
-    return render_template("organizer/dashboard.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
